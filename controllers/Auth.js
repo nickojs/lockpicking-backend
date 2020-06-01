3e7685b9d0f7abb471876586f891fb6bec5d0709
nicko <62088609+nickojs@users.noreply.github.com>
2020-06-01 03:18:04 -0300

    Creating users on the database

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,16 +1,16 @@
 const bcrypt = require('bcrypt');
+const User = require('../models/User');
 
 class Auth {
   async signup(req, res, next) {
     try {
       const { email, username, password } = req.body;
       const hashedPw = await bcrypt.hash(password, 12);
-
-      const user = { email, username, password };
+      const user = await User.create({ username, email, password: hashedPw });
 
       res.status(201).json({
         message: 'successfully created user',
-        user
+        userId: user.id
       });
     } catch (error) {
       next(error);
