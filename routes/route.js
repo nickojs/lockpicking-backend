8dc5ccf13eddb4c4aa6b9a8f7741a3b386577b50
nicko <62088609+nickojs@users.noreply.github.com>
2020-06-01 20:10:05 -0300

    Added validation to new-user route

--- routes/route.js
+++ routes/route.js
@@ -2,14 +2,36 @@ const express = require('express');
 const { body } = require('express-validator');
 const validateResult = require('../helpers/validate-routes');
 
-const router = express.Router();
+const User = require('../models/User');
 const Auth = require('../controllers/Auth');
+const router = express.Router();
 
 // write your routes here
 router.get('/', (req, res, next) => {
   res.send({ message: 'root route' });
 });
 
-router.post('/new-user', Auth.signup);
+router.post('/new-user',
+  [
+    body('username').trim()
+      .isLength({ min: 4, max: 18 })
+      .withMessage('Invalid user length'),
+    body('password')
+      .isLength({ min: 8, max: 20 })
+      .withMessage('Invalid password length')
+      .matches(/([0-9].*[a-zA-Z])|([a-zA-Z].*[0-9])/)
+      .withMessage('Password should have at least one number and one char'),
+    body('email').trim()
+      .normalizeEmail()
+      .isEmail()
+      .withMessage('Invalid email')
+      .custom(async (value) => {
+        const user = await User.findOne({ where: { email: value } });
+        if (user) return Promise.reject();
+      })
+      .withMessage('Email already exists')
+  ],
+  validateResult.default,
+  Auth.signup);
 
 module.exports = router;
