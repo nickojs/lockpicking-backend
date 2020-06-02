2c4ec0483e68e4c8e4c5ac06694f276be2ffaf49
nicko <62088609+nickojs@users.noreply.github.com>
2020-06-02 01:57:19 -0300

    Refactored functions to improve user experience

--- helpers/token-expiration.js
+++ helpers/token-expiration.js
@@ -1,17 +1,21 @@
 const moment = require('moment');
 
-const expirationDates = () => {
+const expirationDates = (expiration) => {
   const startTime = moment();
-  const expiresIn = moment().add(1, 'hour'); // 1 hour from now
-  const diff = moment.duration(expiresIn.diff(startTime));
+  const expiresIn = moment().add(1, 'hour');
+  const diff = expiration
+    ? moment.duration(moment(expiration).diff(startTime))
+    : moment.duration(expiresIn.diff(startTime));
 
   const timeDiff = {
     seconds: diff.get('seconds'),
+    minutes: diff.get('minutes'),
     hours: diff.get('hours')
   };
 
   const timeSulfix = {
     hours: timeDiff.hours > 1 ? 'hours' : 'hour',
+    minutes: timeDiff.minutes > 1 ? 'minutes' : 'minute',
     seconds: timeDiff.seconds > 1 ? 'seconds' : 'second'
   };
 
@@ -23,4 +27,18 @@ const expirationDates = () => {
   return [dateLimits, timeDiff, timeSulfix];
 };
 
-module.exports = expirationDates;
+const expirationMessage = (diff, sulfix) => {
+  let hours = '';
+  if (diff.hours > 0) {
+    hours = `${diff.hours} ${sulfix.hours}, `;
+  }
+
+  return `Your token expires in ${hours
+  }${diff.minutes} ${sulfix.minutes
+  } and ${diff.seconds} ${sulfix.seconds}.`;
+};
+
+module.exports = {
+  expirationDates,
+  expirationMessage
+};
