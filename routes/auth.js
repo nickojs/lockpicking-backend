afc5e0c1d3cefd62cd093fae7dd816955fc7dd11
nicko <62088609+nickojs@users.noreply.github.com>
2020-06-07 08:36:21 -0300

    Using schema validator on auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,47 +1,28 @@
 const express = require('express');
-const { body } = require('express-validator');
+const { body, checkSchema } = require('express-validator');
 const validateResult = require('../helpers/validate-routes');
 
-const User = require('../models/User');
 const Auth = require('../controllers/Auth');
-const isAuth = require('../middlewares/is-auth');
+const { signupSchema } = require('../validators/auth');
 
 const router = express.Router();
 
 router.post('/signup',
-  [
-    body('username').trim()
-      .isLength({ min: 4, max: 18 })
-      .withMessage('Invalid user length')
-      .custom(async (value) => {
-        const user = await User.findOne({ where: { username: value } });
-        if (user) return Promise.reject();
-      })
-      .withMessage('Username already exists'),
-    body('password')
-      .isLength({ min: 8, max: 20 })
-      .withMessage('Invalid password length')
-      .matches(/([0-9].*[a-zA-Z])|([a-zA-Z].*[0-9])/)
-      .withMessage('Password should have at least one number and one char'),
-    body('email').trim()
-      .isEmail()
-      .normalizeEmail()
-      .withMessage('Invalid email')
-      .custom(async (value) => {
-        const user = await User.findOne({ where: { email: value } });
-        if (user) return Promise.reject();
-      })
-      .withMessage('Email already exists')
-  ],
+  checkSchema(signupSchema),
   validateResult.default,
   Auth.signup);
 
-router.post('/login', body('username').trim(), Auth.login);
+router.post('/login',
+  body('username').trim(),
+  Auth.login);
 
 router.post('/request-token/', Auth.setAccountToken);
 
-router.get('/update-user/:userId');
+router.get('/update-user/:token', Auth.getUserData);
 
-router.post('/update-user/:userId', Auth.updateUser);
+router.put('/update-user/:token',
+  checkSchema(signupSchema),
+  validateResult.default,
+  Auth.updateUser);
 
 module.exports = router;
