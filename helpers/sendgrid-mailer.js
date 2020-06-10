f48434d6681c11846ea091fe2a0f3ad27835d1c9
nicko <62088609+nickojs@users.noreply.github.com>
2020-06-10 07:32:02 -0300

    Created more mailing functions, adjusted env filenames

--- helpers/sendgrid-mailer.js
+++ helpers/sendgrid-mailer.js
@@ -3,20 +3,44 @@ const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const mailToken = (email, data) => {
+  // data = username, token, expiration
   const msg = {
     to: email,
     from: process.env.SENDGRID_EMAIL,
-    template_id: process.env.SENDGRID_TOKEN_TEMPLATE,
-    dynamic_template_data: data
+    template_id: process.env.SENDGRID_TOKEN,
+    dynamic_temp: data
   };
   sgMail.send(msg);
 };
 
 const mailNewUser = (email, data) => {
+  // data = username
   const msg = {
     to: email,
     from: process.env.SENDGRID_EMAIL,
-    template_id: process.env.SENDGRID_UPDATE_TEMPLATE,
+    template_id: process.env.SENDGRID_NEW_USER,
+    dynamic_template_data: data
+  };
+  sgMail.send(msg);
+};
+
+const mailUpdatedUser = (email, data) => {
+  // data = currentUser, newUser
+  const msg = {
+    to: email,
+    from: process.env.SENDGRID_EMAIL,
+    template_id: process.env.SENDGRID_UPDATE,
+    dynamic_template_data: data
+  };
+  sgMail.send(msg);
+};
+
+const mailDeletedUser = (email, data) => {
+  // data = username
+  const msg = {
+    to: email,
+    from: process.env.SENDGRID_EMAIL,
+    template_id: process.env.SENDGRID_DELETED_USER,
     dynamic_template_data: data
   };
   sgMail.send(msg);
@@ -24,5 +48,7 @@ const mailNewUser = (email, data) => {
 
 module.exports = {
   mailToken,
-  mailNewUser
+  mailNewUser,
+  mailUpdatedUser,
+  mailDeletedUser
 };
