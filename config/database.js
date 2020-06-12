c9f0ba068b90d8cdd6493d036e85a499cbb20b66
nicko <62088609+nickojs@users.noreply.github.com>
2020-06-12 15:47:20 -0300

    Removed GCP configuration

--- config/database.js
+++ config/database.js
@@ -4,8 +4,8 @@ const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    host: process.env.DB_HOST
   }
 );
 
