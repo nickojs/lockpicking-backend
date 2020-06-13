e6a0581689fdb89504cf6e111b8486bf848cd17f
nicko <62088609+nickojs@users.noreply.github.com>
2020-06-13 14:13:02 -0300

    Adjusted the code back to GCP using local SQL with proxy

--- config/database.js
+++ config/database.js
@@ -5,7 +5,11 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD, {
     dialect: 'mysql',
-    host: process.env.DB_HOST
+    host: process.env.LOCAL_DB
+    // host: '/cloudsql/lockpicking:southamerica-east1:lockpicking',
+    // dialectOptions: {
+    //   socketPath: '/cloudsql/lockpicking:southamerica-east1:lockpicking'
+    // }
   }
 );
 
