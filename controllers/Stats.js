7d97ef6a6035154c2e59756cec2ed440310d8cd8
nicko <62088609+nickojs@users.noreply.github.com>
2020-06-14 08:24:36 -0300

    Added filters to the get route

--- controllers/Stats.js
+++ controllers/Stats.js
@@ -12,6 +12,17 @@ class Stats {
   }
 
   async getStats(req, res, next) {
+    try {
+      // gets last 10 rows from the stats, in the future will receive
+      // query params and fetch those params
+      const stats = await Stat.findAll({ limit: 10, order: [['createdAt', 'DESC']] });
+      if (stats.length <= 0) {
+        res.status(404).json({ message: 'could not find any stats' });
+      }
+      res.status(200).json({ records: stats });
+    } catch (error) {
+      next(error);
+    }
     res.status(200).json({ message: 'stats dude' });
   }
 }
